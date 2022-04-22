2090798ca9757e805e65ee114bb3b1af5fdc7f48
dyjung123 <jeongdongryun@gmail.com>
2022-04-22 20:11:35 +0900

    feature: Add MessageController and CommandObject MessageRequest to send message

---
+++ apache_kafka/src/main/java/com/dongryun/apache_kafka/MessageRequest.java
@@ -0,0 +1,11 @@
+package com.dongryun.apache_kafka;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MessageRequest {
+
+  private String message;
+}
