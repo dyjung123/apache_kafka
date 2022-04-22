0cfaff40cc7d6f955c51e0e8f89c675f259fb131
dyjung123 <jeongdongryun@gmail.com>
2022-04-22 19:12:15 +0900

    Execute send message by commandLineRunner

--- apache_kafka/src/main/java/com/dongryun/apache_kafka/ApacheKafkaApplication.java
+++ apache_kafka/src/main/java/com/dongryun/apache_kafka/ApacheKafkaApplication.java
@@ -1,12 +1,20 @@
 package com.dongryun.apache_kafka;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class ApacheKafkaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApacheKafkaApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+		return args -> kafkaTemplate.send("mytopic", "hello kafka :)");
 	}
 }
